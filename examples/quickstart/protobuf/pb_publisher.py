9427e3e2a95ec732655ed44be803bd4d887585d6
harshith gandhe <harshith@acceldata.io>
2025-02-27 17:29:35 +0530

    ODP-3545 : Fix python shebangs to fix build failure

--- examples/quickstart/protobuf/pb_publisher.py
+++ examples/quickstart/protobuf/pb_publisher.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env ambari-python-wrap
+#!/usr/bin/env python2
 
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
